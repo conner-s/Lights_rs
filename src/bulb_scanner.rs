b32c7eb02b5c33dc6ba9ebcff24174b3538b4bb3
conner <connersmith84@gmail.com>
2019-09-20 16:51:36 -0400

    Basic bulb Scanning, Ready for testing

--- src/bulb_scanner.rs
+++ src/bulb_scanner.rs
@@ -1,9 +1,12 @@
 use std::time;
 use std::net;
 use std::net::{Ipv4Addr};
+use ipaddress::IPAddress;
 
-mod bulb_scanner{
+mod bulb_scanner {
     use super::*;
+    use std::str;
+
 
     struct BulbScanner {
         found_bulbs: Vec<ipaddress::IPAddress>
@@ -12,28 +15,39 @@ mod bulb_scanner{
 
     impl BulbScanner {
 
-        pub fn scan(timeout: time::Duration) -> Vec<Ipv4Addr>{
+        pub fn scan(timeout: time::Duration) -> Vec<IPAddress> {
+            let mut found_bulbs: Vec<IPAddress> = [].to_vec();
 
             //Sets discovery socket address to Broadcast IP and to the port from flux_LED python API
-            let discoveryAdd = Ipv4Addr::BROADCAST.to_string() + "48899";
+            let discovery_add = Ipv4Addr::BROADCAST.to_string() + "48899";
 
-            let sock = net::UdpSocket::bind(discoveryAdd)
+            let sock = net::UdpSocket::bind(&discovery_add)
                 .expect("Broadcast socket did not bind");
 
             let now = time::Instant::now();
-            let mut found_bulbs: Vec<Ipv4Addr> = [].to_vec();
 
-            let msg: str = HF-A11ASSISTHREAD;
+
+            let msg: String = "HF-A11ASSISTHREAD".to_string();
+
             let b_msg = msg.as_bytes();
 
+            let mut post_scan_vec: Vec<String> = Vec::new();
+            let mut buffer = [0; 64];
+            while now.elapsed() < timeout {
+                sock.send_to(b_msg, &discovery_add).expect("Could not send packet");
+                let (bytes_received, src_addr) = sock.recv_from(&mut buffer)
+                    .expect("Did not receive data gram");
+                let filled_buf = &mut buffer[..bytes_received];
+                let buff_to_ascii = filled_buf.to_ascii_lowercase();
+                let data_split = str::from_utf8(&buff_to_ascii).unwrap().split(',');
+                let data_vec = data_split.collect();
+                post_scan_vec.push(data_vec);
+            }
 
-            while now.elapsed() < timeout{
-                sock.send_to(b_msg, &discoveryAdd);
-
-                loop{
-                    let mut buffer = [0; 64];
-                    let (bytes_received, src_addr) = sock.recv_from(&mut buffer);
-                }
+            for addr in post_scan_vec {
+                let push_address= ipaddress::ipv4::new(addr)
+                    .expect("could not parse address");
+                found_bulbs.push(push_address);
             }
             found_bulbs
         }
@@ -41,7 +55,7 @@ mod bulb_scanner{
     }
 }
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
 }
