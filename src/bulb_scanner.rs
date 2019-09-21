c55b41f4bb5bb110089f02e6c93bde5ca063a7bd
Conner <32659799+conner-s@users.noreply.github.com>
2019-09-21 01:25:42 -0400

    Scanner can ping LED Controller and return IP

--- src/bulb_scanner.rs
+++ src/bulb_scanner.rs
@@ -1,61 +1,62 @@
 use std::time;
 use std::net;
-use std::net::{Ipv4Addr};
-use ipaddress::IPAddress;
+use std::str;
+use std::net::Ipv4Addr;
+use std::str::FromStr;
 
-mod bulb_scanner {
-    use super::*;
-    use std::str;
-
-
-    struct BulbScanner {
-        found_bulbs: Vec<ipaddress::IPAddress>
-
-    }
-
-    impl BulbScanner {
-
-        pub fn scan(timeout: time::Duration) -> Vec<IPAddress> {
-            let mut found_bulbs: Vec<IPAddress> = [].to_vec();
-
-            //Sets discovery socket address to Broadcast IP and to the port from flux_LED python API
-            let discovery_add = Ipv4Addr::BROADCAST.to_string() + "48899";
-
-            let sock = net::UdpSocket::bind(&discovery_add)
-                .expect("Broadcast socket did not bind");
-
-            let now = time::Instant::now();
+pub fn scan(timeout: time::Duration) -> net::Ipv4Addr {
+    //Declaring return value to be able to keep it in scoop in the loop
+    let mut ret_addr: Ipv4Addr = Ipv4Addr::new(0, 0, 0, 0);
 
 
-            let msg: String = "HF-A11ASSISTHREAD".to_string();
+    //Sets discovery socket address to Broadcast IP and to the port from flux_LED python API
+    let sock = net::UdpSocket::bind("0.0.0.0:48899")
+        .expect("Socket did not bind");
 
-            let b_msg = msg.as_bytes();
+    //Takes current time for timeout purposes
+    let now = time::Instant::now();
 
-            let mut post_scan_vec: Vec<String> = Vec::new();
-            let mut buffer = [0; 64];
-            while now.elapsed() < timeout {
-                sock.send_to(b_msg, &discovery_add).expect("Could not send packet");
-                let (bytes_received, src_addr) = sock.recv_from(&mut buffer)
-                    .expect("Did not receive data gram");
-                let filled_buf = &mut buffer[..bytes_received];
-                let buff_to_ascii = filled_buf.to_ascii_lowercase();
-                let data_split = str::from_utf8(&buff_to_ascii).unwrap().split(',');
-                let data_vec = data_split.collect();
-                post_scan_vec.push(data_vec);
-            }
 
-            for addr in post_scan_vec {
-                let push_address= ipaddress::ipv4::new(addr)
-                    .expect("could not parse address");
-                found_bulbs.push(push_address);
-            }
-            found_bulbs
+    //Declares message to send to broadcast to LED controller
+    let msg: String = "HF-A11ASSISTHREAD".to_string();
+    let b_msg = msg.as_bytes();
+    let mut buffer = [0; 64];
+
+    //Broadcasts
+    sock.send_to(b_msg, "192.168.0.255:48899")
+        .expect("Could not send packet");
+
+    //Setting timout for socket method
+    sock.set_read_timeout(Option::from(timeout))
+        .expect("Could not set timout");
+
+
+    loop {
+        //Timeout Break case
+        if now.elapsed().as_secs() > timeout.as_secs() {
+            break;
         }
 
+        //Checking for packets to receive
+        let (bytes_received, src_addr) = sock.recv_from(&mut buffer)
+            .expect("Did not receive data gram");
+        //unwrapping pack info to text
+        let filled_buf = &mut buffer[..bytes_received];
+        let buff_to_ascii = filled_buf.to_ascii_lowercase();
+        let bulb_data = str::from_utf8(&buff_to_ascii).unwrap();
+
+        //Checks to see if the packet is from the LED controller
+        if !bulb_data.contains("hf-a11assisthread") {
+            let chop = src_addr.ip().to_string();
+            ret_addr = Ipv4Addr::from_str(&chop[0..])
+                .expect("Could not convert to ipv4");
+            break;
+        }
     }
+    ret_addr
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::time::Duration;
+use std::net::Ipv4Addr;
+
 mod bulb_scanner;
 mod wifi_led_bulb;
 
 fn main() {
+    let bulb: Ipv4Addr = bulb_scanner::scan(Duration::from_secs(6));
+    println!("{}", bulb.to_string());
 
 }
 
